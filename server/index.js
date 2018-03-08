0c3113e9f0858ab4d00794279656589fe8444215
TV <trungvuh@gmail.com>
2018-03-07 23:19:48 -0800

    Testing

--- server/index.js
+++ server/index.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 
 // If there is an environment provided by Heroku, use it, otherwise, listen to 5000
 const PORT = process.env.PORT || 5000;
-app.listen(5000); //instruct Express to tell Node to listen to incoming traffic
+app.listen(PORT); //instruct Express to tell Node to listen to incoming traffic
