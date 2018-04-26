31fe93bb921afa4c836e1ac23fb67fbcaa7cd4bf
TV <trungvuh@gmail.com>
2018-04-26 07:47:43 -0700

    Setting up Mongo and Mongoose

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 // generate a new application represent a running express app
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/user');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
 //require the authRoutes returns a function, immediately call that fnct, passing in the app object
 
 app.get('/', (req, res) => {
-  res.send({ hello: 'worldsss' });
+  res.send({ hello: 'world' });
 });
 
 //If there is an environment provided by Heroku, use it, otherwise, listen to 5000
