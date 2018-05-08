d3db16f10678df08ec4b6e7cb02473a4de0eeefc
TV <trungvuh@gmail.com>
2018-05-07 20:37:38 -0700

    Reducer

--- services/passport.js
+++ services/passport.js
@@ -25,18 +25,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have the record with given profile Id
-          done(null, existingUser);
-        } else {
-          // no profile Id exists, create new one
-          new User({ googleId: profile.id }) //create a Mongoose model instance
-            .save() // save it to our MongoDB database
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // we already have the record with given profile Id
+        return done(null, existingUser);
+      }
+      // no profile Id exists, create new one
+      // create a Mongoose model instance
+      const user = await new User({ googleId: profile.id }).save(); // save it to our MongoDB database
+      done(null, user);
     }
   )
 );
